c885ea871f4ff1f9981dc9eff951c49f36234d20
Milad Bonakdar <miladbonak@gmail.com>
2016-05-12 01:37:28 +0430

    home forum view

--- Forum/partials/Forum/_Home/HomeCtrl.js
+++ Forum/partials/Forum/_Home/HomeCtrl.js
@@ -1,15 +1,15 @@
-angular.module(appName).controller('HomeCtrl', function ($scope, $element, $rootScope, $routeParams, $state, $location, $timeout, Extention) {
+﻿angular.module(appName).controller('HomeCtrl', function ($scope, $element, $rootScope, $routeParams, $state, $location, $timeout, Extention) {
 
     $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Series A', 'Series B'];
-    $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
+    $scope.series = ['Series A', 'Series b'];
+    $scope.seriess = ['Series A', 'Series b'];
+    $scope.data = [[28, 48, 40, 19, 40, 19 , 65],
+      [65, 59, 80, 81, 56, 55, 40]
     ];
-    $scope.labelss = ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"];
+    $scope.labelss = ["دیتا سوئیچ", "رادیویی", "سیستم های انتقال", "خطوط انتقال"];
     $scope.datas = [
-      [65, 59, 90, 81, 56, 55, 40],
-      [28, 48, 40, 19, 96, 27, 100]
+      [28, 48, 40, 19],
+      [65, 59, 90, 81]
     ];
     $scope.onClick = function (points, evt) {
         console.log(points, evt);
