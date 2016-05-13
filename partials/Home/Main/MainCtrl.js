d64a41eb759d53497e9fe051524769629b099369
Milad Bonakdar <miladbonak@gmail.com>
2016-05-13 12:05:34 +0430

    main page changed

--- partials/Home/Main/MainCtrl.js
+++ partials/Home/Main/MainCtrl.js
@@ -86,8 +86,7 @@
     }
 
     $scope.mainOptions = {
-        sectionsColor: ['#1BBC9B', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-        anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+        anchors: ['firstPage'/*, 'secondPage', '3rdPage', '4thpage', 'lastPage'*/],
         menu: '#menu'
     };
 
@@ -95,31 +94,31 @@
     
 
     //TODO slide properties : 
-    $scope.slides = [
-        {
-            subject: 'رادیویی',
-            image: 'Images/broadcast.png',
-            title: 'Simple',
-            description: 'Easy to use. Configurable and customizable.',
-            src: 'images/1.png'
-        },
-        {
-            anchors:'testSlide',
-            title: 'Cool',
-            description: 'It just looks cool. Impress everybody with a simple and modern web design!',
-            src: 'images/2.png'
-        },
-        {
-            title: 'Compatible',
-            description: 'Working in modern and old browsers too!',
-            src: 'images/3.png'
-        },
-        {
-            title: 'Compatible',
-            description: 'Working in modern and old browsers too!',
-            src: 'images/3.png'
-        }
-    ];
+    //$scope.slides = [
+    //    {
+    //        subject: 'رادیویی',
+    //        image: 'Images/broadcast.png',
+    //        title: 'Simple',
+    //        description: 'Easy to use. Configurable and customizable.',
+    //        src: 'images/1.png'
+    //    },
+    //    {
+    //        anchors:'testSlide',
+    //        title: 'Cool',
+    //        description: 'It just looks cool. Impress everybody with a simple and modern web design!',
+    //        src: 'images/2.png'
+    //    },
+    //    {
+    //        title: 'Compatible',
+    //        description: 'Working in modern and old browsers too!',
+    //        src: 'images/3.png'
+    //    },
+    //    {
+    //        title: 'Compatible',
+    //        description: 'Working in modern and old browsers too!',
+    //        src: 'images/3.png'
+    //    }
+    //];
 
     //$scope.addSlide = function () {
     //    $scope.slides.push({
